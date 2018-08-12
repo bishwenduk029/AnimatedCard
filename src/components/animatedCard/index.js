a7e9673ae0dedd811a6e7524f4f495df6a99ba69
Bishwendu Kundu <bishwenduk029@gmail.com>
2018-08-12 13:34:26 +0530

    Make the component re-usable
    Allow component to be re-used in any context responsively

--- src/components/animatedCard/index.js
+++ src/components/animatedCard/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { CardLayout, CardBody, CardFooter } from "./style";
+import {
+  CardLayout,
+  CardBody,
+  CardFooter,
+  TextBody,
+  ImageContent
+} from "./style";
 
 class AnimatedCard extends React.Component {
   state = {
-    expand: false,
-    cardHeight: 300
+    expand: false
   };
 
   toggelHeight = () => {
@@ -14,8 +19,8 @@ class AnimatedCard extends React.Component {
     }));
   };
 
-  expandCardHeight = (byHeight = 200) => {
-    return this.state.cardHeight + byHeight;
+  expandCardHeight = (originalHeight = 0, byHeight = 200) => {
+    return originalHeight + byHeight;
   };
 
   render() {
@@ -23,15 +28,19 @@ class AnimatedCard extends React.Component {
     return (
       <CardLayout
         height={
-          this.state.expand ? this.expandCardHeight(125) : this.state.cardHeight
+          this.state.expand
+            ? this.expandCardHeight(this.props.cardHeight, 50)
+            : this.props.cardHeight
         }
       >
         <CardBody>
-          <img
-            width="100%"
-            height="100%"
-            src="https://cdn-images-1.medium.com/max/1200/1*SL4sWHdjGR3vo0x5ta3xfw.jpeg"
-          />
+          <ImageContent>{this.props.imageContent()}</ImageContent>
+          <TextBody
+            yShift={this.state.expand ? 0 : -120}
+            fade={this.state.expand}
+          >
+            {this.props.textContent}
+          </TextBody>
         </CardBody>
         <CardFooter
           onClick={this.toggelHeight}
@@ -47,10 +56,17 @@ class AnimatedCard extends React.Component {
 export default AnimatedCard;
 
 AnimatedCard.propTypes = {
-  footerSvg: PropTypes.element
+  footerSvg: PropTypes.element,
+  textContent: PropTypes.string
 };
 
 AnimatedCard.defaultProps = {
+  imageContent: props => (
+    <img
+      width="100%"
+      src="https://cdn-images-1.medium.com/max/1200/1*SL4sWHdjGR3vo0x5ta3xfw.jpeg"
+    />
+  ),
   footerSvg: props => (
     <svg
       class="chevron"
@@ -60,5 +76,7 @@ AnimatedCard.defaultProps = {
     >
       <path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5" />
     </svg>
-  )
+  ),
+  textContent: "Almost 3 years of experience and rate myself 3.5/5",
+  cardHeight: 225
 };
--- src/components/animatedCard/style.js
+++ src/components/animatedCard/style.js
@@ -3,27 +3,38 @@ import styled from "react-emotion";
 export const CardLayout = styled("div")`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   background: white;
   border-radius: 8px;
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   cursor: pointer;
   height: ${props => `${props.height}px`};
   padding: 0 20px;
-  -webkit-tap-highlight-color: rgba(0,0,0,0.025);
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0.025);
   text-align: center;
   transition: height 385ms ease-in;
   width: 260px;
 `;
 
 export const CardBody = styled("div")`
+  display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 85%;
 `;
 
 export const CardFooter = styled("div")`
-  height: 15%;
-  padding-top: 5%;
   transform: ${props => `rotate(${props.rotate}deg)`};
-  transition: transform 225ms 15ms ease-in;
+  transition: transform 325ms 15ms ease-out;
+`;
+
+export const TextBody = styled("p")`
+  transform: ${props => `translateY(${props.yShift}px)`};
+  color: ${props => `rgba(0, 0, 0, ${props.fade ? 1 : 0})`};
+  transition: color 300ms 50ms ease-in,
+    transform 325ms 18ms cubic-bezier(0.65, 0.05, 0.36, 1);
+`;
+
+export const ImageContent = styled("div")`
+  width: 100%;
+  z-index: 1;
 `;
